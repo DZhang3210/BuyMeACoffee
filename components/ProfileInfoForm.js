7202be64f4809f2e4cbf3ea400ccd66562043096
DZhang3210 <53838139+DZhang3210@users.noreply.github.com>
2024-07-12 15:35:27 -0400

    fixed bug

--- components/ProfileInfoForm.js
+++ components/ProfileInfoForm.js
@@ -5,6 +5,8 @@ import ProfileImageForm from './ProfileImageForm'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { redirect } from 'next/dist/server/api-utils';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Terminal } from 'lucide-react';
 
 const ProfileInfoForm = () => {
   const [userName, setUserName] = useState('')
@@ -12,11 +14,18 @@ const ProfileInfoForm = () => {
   const [bio, setBio] = useState('')
   const [avatarURL, setAvatarURL] = useState('')
   const [coverURL, setCoverURL] = useState('')
+  const [showPop, setShowPop] = useState(false)
 
   async function handleFormAction(e){
     e.preventDefault()
     const formData = new FormData(e.target);
     const response = await saveProfile(formData)
+    setShowPop(true)
+    try{
+        new Promise(resolve => setTimeout(()=>{setShowPop(false)},3000))
+    }catch(err){
+        console.log(err)
+    }
   }
   useEffect((()=>{
     async function getProfile (){
@@ -38,6 +47,14 @@ const ProfileInfoForm = () => {
   }),[])
   
   return (
+    <>
+    {showPop && <Alert className = "fixed bottom-0 left-auto right-auto">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+            You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+    </Alert>}
     <form
         onSubmit = {handleFormAction}
     >
@@ -45,15 +62,15 @@ const ProfileInfoForm = () => {
             
         <div>
             <label className = "block mt-4" htmlFor='usernameIn'>username:</label>
-            <input name = "username" defaultValue = {userName} type = "text" placeholder='username' id = "usernameIn"></input>
+            <input name = "username" defaultValue = {userName} type = "text" placeholder='username' id = "usernameIn" className='w-full border'></input>
         </div>
         <div>
             <label className = "block mt-4" htmlFor='displayNameIn'>display name:</label>
-            <input name = "displayName" defaultValue = {displayName} type = "text" placeholder='display name' id ="displayNameIn"></input>
+            <input name = "displayName" defaultValue = {displayName} type = "text" placeholder='display name' id ="displayNameIn" className='w-full border'></input>
         </div>
         <div>
             <label className = "block mt-4" htmlFor='bioIn'>bio:</label>
-            <textarea name = "bio" defaultValue = {bio} placeholder='bio' id ="bioIn"></textarea>
+            <textarea name = "bio" defaultValue = {bio} placeholder='bio' id ="bioIn" className='w-full border'></textarea>
         </div>
         <div>
             <button 
@@ -64,6 +81,7 @@ const ProfileInfoForm = () => {
             </button>
         </div>
     </form>
+    </>
   )
 }
 
